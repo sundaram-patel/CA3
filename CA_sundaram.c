fe82cfa4724069260ffc34eff0b5b33d9c1b22f4
sundaram-patel <38398158+sundaram-patel@users.noreply.github.com>
2018-04-15 18:01:30 +0530

    implement2

--- CA_sundaram.c
+++ CA_sundaram.c
@@ -26,4 +26,37 @@ process_structure temp;
 printf("Enter Total Number of Processes:\t");
 scanf("%d", &limit);
 process_structure process[limit];
+for(count = 0; count < limit; count++)
+{
+printf("\nProcess ID:\t");
+scanf("%d", &process[count].pro_id);
+printf("Arrival Time:\t");
+scanf("%d", &process[count].at);
+printf("Burst Time:\t");
+scanf("%d", &process[count].bt);
+printf("Process pt:\t");
+scanf("%d", &process[count].pt);
+tp = process[count].pt;
+process[count].q = Queue(tp);
+process[count].ready = 0;
+}
+time = process[0].bt;
+for(y = 0; y < limit; y++)
+{
+for(count = y; count < limit; count++)
+{
+if(process[count].at < time)
+{
+process[count].ready = 1;
+}}
+for(count = y; count < limit - 1; count++)
+{
+for(r = count + 1; r < limit; r++)
+{
+if(process[count].ready == 1 && process[r].ready == 1)
+{
+if(process[count].q == 2 && process[r].q == 1)
+{
+temp = process[count];
+process[count] = process[r];
 
